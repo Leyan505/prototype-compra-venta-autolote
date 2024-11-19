286fd78f09462d3b4afb587d262efc2bfa63f079
Leyan505 <leyan50505@gmail.com>
2024-11-19 13:53:47 -0600

    Add dashboard.html, .rs and js files

---
+++ src/dashboard.rs
@@ -0,0 +1,14 @@
+use std::string;
+use std::fmt;
+use crate::AppState;
+use actix_web::{error::InternalError, get, http::{header::FROM, StatusCode}, post, web::{self, Json, Redirect}, HttpResponse, Responder, ResponseError};
+use chrono::NaiveDate;
+use serde::{Deserialize, Serialize};
+use sqlx::{self, types::{chrono, BigDecimal}, FromRow};
+use tera::{Context, Tera};
+
+#[get("/")]
+pub async fn show(state: web::Data<AppState>, tera: web::Data<Tera>) -> impl Responder{
+    let mut context = Context::new();
+    HttpResponse::Ok().body(tera.render("index.html", &context).unwrap())
+}
